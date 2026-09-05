93ffdf4abdbb8eba17617a48881dc819e81f29d2
Dominik Rimpf <dev@drimpf.de>
2026-09-05 23:55:15 +0000

    kituserlookup: add 2.3.2 bottle.

--- Formula/kituserlookup.rb
+++ Formula/kituserlookup.rb
@@ -5,6 +5,12 @@ class Kituserlookup < Formula
   sha256 "54433caafb744fee73ead0ea61d1afe009e1ff9a5e74e2a69a7b13bc038c9763"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/domrim/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "e8a0736e20a2b0e29c9658d46d06f6949213e08023712ccbba6a5c44d65d7bab"
+    sha256 cellar: :any,                 x86_64_linux: "1337617be4cca51f4cf1275360712d9f5a3f2cae041e1e5e039d13f88d8a3926"
+  end
+
   depends_on "go" => :build
 
   deny_network_access!
